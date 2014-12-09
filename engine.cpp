642ab5250a80feec98c91a8d76107a7df674e47b
LeVirus <charbonneau_cyril@hotmail.fr>
2014-12-09 12:18:42 +0000

    Ajout quelque commentaires doxygen

--- engine.cpp
+++ engine.cpp
@@ -1,5 +1,30 @@
 #include "engine.hpp"
 
-Engine::Engine()
-{
+/**
+ * @brief Engine::Engine Constructeur de la classe Engine.
+ */
+Engine::Engine(){
+
+}
+
+/**
+ * @brief Engine::bAddEntity Fonction d'ajout d'une entité dans le conteneur.
+ * @param uiIdEntity Le numéro d'identifiant unique qui sera attribué à l'entité.
+ * @return true si l'entité a été créée avec succés, false sinon.
+ */
+bool Engine::bAddEntity( unsigned int uiIdEntity ){
+
+}
+
+/**
+ * @brief Engine::bRmEntity Fonction de suppression d'une entité dans le conteneur.
+ * @param uiIdEntity Le numéro d'identifiant unique qui sera attribué à l'entité.
+ * @return true si l'entité a été supprimé avec succés, false sinon.
+ */
+bool Engine::bRmEntity( unsigned int uiIdEntity ){
+
+}
+
+bool Engine::bRmAllEntity(){
+
 }
