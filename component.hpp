623dcb68e4a43321d156b3790d43146493c78318
LeVirus <charbonneau_cyril@hotmail.fr>
2014-12-10 11:29:15 +0000

    Save modif

--- component.hpp
+++ component.hpp
@@ -1,4 +1,14 @@
 #ifndef COMPONENT_HPP
 #define COMPONENT_HPP
 
+/**
+ * @brief La classe Component est la classe mère de tous les composants. 
+ * Un composant représente un élément d'une entité.
+ * Elle est abstraite, et comporte un identifiant de type.
+ * Tous les éléments de la classe sont public
+ */
+abstract struct Component{
+    unsigned int muiTypeComponent;
+};
+
 #endif // COMPONENT_HPP
