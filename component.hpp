afb854a04dfa7e3e6bad77f243b514765bb373aa
Charbonneau <cyril@localhost.localdomain>
2014-12-10 15:47:24 +0100

    Component class ==> struct.

--- component.hpp
+++ component.hpp
@@ -2,13 +2,14 @@
 #define COMPONENT_HPP
 
 /**
- * @brief La classe Component est la classe mère de tous les composants. 
+ * @brief La structure Component est la classe mère de tous les composants.
  * Un composant représente un élément d'une entité.
  * Elle est abstraite, et comporte un identifiant de type.
- * Tous les éléments de la classe sont public
  */
-abstract struct Component{
-    unsigned int muiTypeComponent;
+struct Component{
+public:
+
+    unsigned int muiTypeComponent, muiIDComponent;
 };
 
 #endif // COMPONENT_HPP
