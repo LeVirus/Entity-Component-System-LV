c44cc261c961fcf1de6ae109fffa52b0ae6a0a37
LeVirus <charbonneau_cyril@hotmail.fr>
2015-04-28 18:55:12 +0000

    Implementation debut CollSystem::execSysteme.

--- collisionsystem.cpp
+++ collisionsystem.cpp
@@ -1,6 +1,10 @@
 #include "collisionsystem.hpp"
 #include "ECSconstantes.hpp"
 #include "system.hpp"
+#include "physicscomponent.hpp"
+#include "entity.hpp"
+#include "engine.hpp"
+#include <cassert>
 
 /**
  * @brief CollisionSystem::CollisionSystem Constructeur de CollisionSystem.
@@ -27,12 +31,70 @@ void CollisionSystem::bModifyNumberTag( unsigned int uiNumTag ){
 }
 
 /**
- * @brief CollisionSystem::execSystem
+ * @brief CollisionSystem::execSystem Fonction vérifiant si chaque entité est en collision avec les autres.
+ * Un tableau stocke les résultat.
  */
-void CollisionSystem::execSystem() override{
+void CollisionSystem::execSystem(){
     const std::vector< Entity > & vectEntity = mptrSystemManager -> getptrEngine() -> getVectEntity();
+
     System::execSystem();
 
+    for( unsigned int i = 0; i < mVectNumEntity . size() - 1; ++i ){
+        //mBitSet2dTagCollision.
+
+
+        for( unsigned int j = i + 1; j < mVectNumEntity . size(); ++j ){
+            bEntityIsInCollision( i, j );
+        }
+    }
+
+}
+
+/**
+ * @brief CollisionSystem::bEntityIsInCollision Fonction vérifiant si 2 entités sont en collision.
+ * La fonction vérifie d'abord si les tags de chaque entité corresponde.
+ * @param uiEntityA Le numéro de la première entité.
+ * @param uiEntityB Le numéro de la deuxième entité.
+ * @return false si les 2 entités ne sont pas en collision ou si les tags de ces dernières ne sont pas correspondant,
+ * true sinon.
+ */
+bool CollisionSystem::bEntityIsInCollision( unsigned int uiEntityA, unsigned int uiEntityB ){
+    const std::vector< Entity > & vectEntity = mptrSystemManager -> getptrEngine() -> getVectEntity();
+
+    unsigned int uiTabEntitySize =  vectEntity . size();
+
+    assert( ( uiEntityA < uiTabEntitySize && uiEntityB < uiTabEntitySize ) && "Num Entity hors tableau.\n" );
+
+    if( ! bEntityTagMatches( uiEntityA, uiEntityB ) ){
+        return false;
+    }
+    //recup des 2 bitset des 2 entités
+    const std::bitset< NUMBR_COMPONENT > & entityBitsetA = vectEntity[ uiEntityA ] . getEntityBitSet(),
+           & entityBitsetB = vectEntity[ uiEntityB ] . getEntityBitSet();
+
+    for( unsigned int i = 0; i < entityBitsetA . size() - 1; ++i ){
+        for( unsigned int j = i + 1; j < entityBitsetB . size(); ++j ){
+
+        }
+    }
+}
+
+/**
+ * @brief CollisionSystem::bEntityTagMatches Fonction vérifiant dans le tableau du système( mBitSet2dTagCollision ) si les 2 tags des
+ * 2 entités sont corespondant.
+ * Un assert est appelé si au moins une des 2 entités ne possèdent pas le composant PHYSIC_COMPONENT.
+ * @param uiEntityA Le numéro de la première entité.
+ * @param uiEntityB Le numéro de la deuxième entité.
+ * @return true si les tags correspondent, false sinon.
+ */
+bool CollisionSystem::bEntityTagMatches( unsigned int uiEntityA, unsigned int uiEntityB ){
+    PhysicComponent * physicComponentEntityA = stairwayToComponentManager() .
+            searchComponentByType< PhysicComponent >( uiEntityA, PHYSIC_COMPONENT ) ,
+
+            * physicComponentEntityB = stairwayToComponentManager() .
+                    searchComponentByType< PhysicComponent >( uiEntityB, PHYSIC_COMPONENT );
+    assert( ( physicComponentEntityA && physicComponentEntityB ) && "PhysicComp non instancié.\n" );
+    return mBitSet2dTagCollision . getValAt( physicComponentEntityA -> muiTag, physicComponentEntityB -> muiTag );
 }
 
 /**
--- collisionsystem.hpp
+++ collisionsystem.hpp
@@ -2,20 +2,23 @@
 #define COLLISIONSYSTEM_H
 
 #include "bitset2d.hpp"
+#include "system.hpp"
 
 /**
  * @brief La classe CollisionSystem détermine les entités qui sont en collisions.
  * Un tableau détermine quelles type d'entité peut entrer en collision avec d'autres.
  */
-class CollisionSystem{
+class CollisionSystem : System{
 private:
-    BitSet2d mBitSet2dTagCollision;
+    BitSet2d mBitSet2dTagCollision, mBitSet2dInCollision;
     unsigned int muiNumberTag;
 public:
     CollisionSystem();
     void bModifyNumberTag( unsigned int uiNumTag );
     void execSystem() override;
-    bool attributeVectTabTagCollision(const std::vector< bool > & vectBool, unsigned int uiLenght );
+    bool bEntityIsInCollision( unsigned int uiEntityA, unsigned int uiEntityB );
+    bool bEntityTagMatches( unsigned int uiEntityA, unsigned int uiEntityB );
+    bool attributeVectTabTagCollision( const std::vector< bool > & vectBool, unsigned int uiLenght );
 };
 
 #endif // COLLISIONSYSTEM_H
